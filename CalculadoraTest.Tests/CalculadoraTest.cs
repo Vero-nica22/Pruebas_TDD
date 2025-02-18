628acd4dc51d68a3b0f4b3cc91a6b0de384e81a4
ximena <ximenazamudio2301@gmail.com>
2025-02-18 11:27:48 -0500

    Pruebas para resta y multiplicacion

---
+++ CalculadoraTest.Tests/CalculadoraTest.cs
@@ -0,0 +1,42 @@
+using Xunit;
+using CalculadoraTest; 
+
+namespace CalculadoraTest.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Resta()
+        {
+            //Inicializar los datos
+            Calculadora calculadora = new Calculadora();
+            int numero1 = 10;
+            int numero2 = 5;
+            int resultado = 5;
+
+            //Probar codigo
+            int resultadoReal = calculadora.Restar(numero1, numero2); 
+
+            //Verificacion
+            Assert.Equal(resultado, resultadoReal);
+        }
+
+        [Fact]
+        public void Multiplicacion()
+        {
+            // Inicializacion
+            Calculadora calculadora = new Calculadora();
+            int num1 = 3;
+            int num2 = 4;
+            int resultado = 12;
+
+            //Prueba
+            int resultadoReal = calculadora.Multiplicar(num1, num2); // Llama al método Multiplicar de tu calculadora
+
+            //Verificacion
+            Assert.Equal(resultado, resultadoReal);
+        }
+
+
+    }
+}
